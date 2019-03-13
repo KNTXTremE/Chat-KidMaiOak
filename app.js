5a592dfc091799856278dc2a744aeb495a22d628
Chalermwat <zi.sk134@gmail.com>
2019-03-13 15:30:34 +0700

    HTTP version

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var socketIO = require('socket.io');
+var io = require('socket.io')(http);
 var port = 8080;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(port, function(){
-    console.log('listening on *: '+port);
-  });
-
-var io = socketIO.listen(server)
 io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-  socket.on('chat message', (msg)=>{
-      console.log('Message : '+msg);
-  })
-
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-
 });
 
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
